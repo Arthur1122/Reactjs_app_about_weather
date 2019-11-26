7e18c6b69d399da480d833dfc5f1552640c02f7c
Artur Petrosyan <arm.arturpetrosyan@gmail.com>
2019-11-26 17:00:30 +0400

    Read weather from weather API and created from

--- src/App.js
+++ src/App.js
@@ -4,12 +4,29 @@ import Titles from "./components/Titles";
 import Form from "./components/Form";
 import Weather from "./components/Weather";
 
+const API_KEY = "3585775f387b0d0cba6c5b3dc41b8167";
+
 class App extends React.Component {
+  getWeather = async e => {
+    e.preventDefault();
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+    const api_call = await fetch(
+      "http://api.openweathermap.org/data/2.5/weather?q=" +
+        city +
+        "," +
+        country +
+        "&APPID=0cc05788446f27a0f300d1044102a320"
+    );
+    const data = await api_call.json();
+    console.log(data);
+  };
+
   render() {
     return (
       <div>
         <Titles />
-        <Form />
+        <Form getWeather={this.getWeather} />
         <Weather />
       </div>
     );
--- src/components/Form.js
+++ src/components/Form.js
@@ -2,7 +2,15 @@ import React from "react";
 
 class Form extends React.Component {
   render() {
-    return <div>Form Component</div>;
+    return (
+      <div>
+        <form onSubmit={this.props.getWeather}>
+          <input type="text" name="city" placeholder="city ..." />
+          <input type="text" name="country" placeholder="country ..." />
+          <button>Get Weather</button>
+        </form>
+      </div>
+    );
   }
 }
 
