85a01962f17ccca4ea8f456f5134b21c097d72c1
Artur Petrosyan <arm.arturpetrosyan@gmail.com>
2019-11-26 23:07:03 +0400

    short design reutrn component

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,20 +1,19 @@
 import React from "react";
 
-const Weather = props => {
-  return (
-    // stex stugvuma ete orinak city === true (aysinkn datark chi ) && erkrord city === true tpuma Location ... hakarak depkum ban chi tpum
-    <div>
-      {props.city && props.country && (
-        <p>
-          Location : {props.city}, {props.country}
-        </p>
-      )}
-      {props.temperature && <p>Temperature : {props.temperature}</p>}
-      {props.humidity && <p>Humidity : {props.humidity}</p>}
-      {props.description && <p>Conditions : {props.description}</p>}
-      {props.erorr && <p>{props.erorr}</p>}
-    </div>
-  );
-};
+//erb vor poxancvuma mi parametr u veradarcvuma mi blok <div> karank senc grenk
+const Weather = props => (
+  // stex stugvuma ete orinak city === true (aysinkn datark chi ) && erkrord city === true tpuma Location ... hakarak depkum ban chi tpum
+  <div>
+    {props.city && props.country && (
+      <p>
+        Location : {props.city}, {props.country}
+      </p>
+    )}
+    {props.temperature && <p>Temperature : {props.temperature}</p>}
+    {props.humidity && <p>Humidity : {props.humidity}</p>}
+    {props.description && <p>Conditions : {props.description}</p>}
+    {props.erorr && <p>{props.erorr}</p>}
+  </div>
+);
 
 export default Weather;
