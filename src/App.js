cc9c904deb6a3ce702d53cb81251137acb7dd3c7
Artur Petrosyan <arm.arturpetrosyan@gmail.com>
2019-11-26 22:57:46 +0400

    Get data and show those

--- src/App.js
+++ src/App.js
@@ -7,8 +7,17 @@ import Weather from "./components/Weather";
 const API_KEY = "0cc05788446f27a0f300d1044102a320";
 
 class App extends React.Component {
+  state = {
+    temperature: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    description: undefined,
+    erorr: undefined
+  };
+
   getWeather = async e => {
-    e.preventDefault();
+    e.preventDefault(); // esi grvuma for kancheluc update chli eje
     const city = e.target.elements.city.value;
     const country = e.target.elements.country.value;
     const api_call = await fetch(
@@ -20,7 +29,28 @@ class App extends React.Component {
         API_KEY
     );
     const data = await api_call.json();
-    console.log(data);
+    //stex stugvuma if (city && country mej value ka) mtnum poxuma state else(talisa erorr)
+    if (city && country) {
+      console.log(data);
+
+      this.setState({
+        temperature: data.main.temp,
+        city: data.name,
+        country: data.sys.country,
+        humidity: data.main.humidity,
+        description: data.weather[0].description,
+        erorr: ""
+      });
+    } else {
+      this.setState({
+        temperature: undefined,
+        city: undefined,
+        country: undefined,
+        humidity: undefined,
+        description: undefined,
+        erorr: "Please enter the value."
+      });
+    }
   };
 
   render() {
@@ -28,7 +58,14 @@ class App extends React.Component {
       <div>
         <Titles />
         <Form getWeather={this.getWeather} />
-        <Weather />
+        <Weather
+          temperature={this.state.temperature}
+          city={this.state.city}
+          country={this.state.country}
+          humidity={this.state.humidity}
+          description={this.state.description}
+          erorr={this.state.erorr}
+        />
       </div>
     );
   }
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -2,7 +2,22 @@ import React from "react";
 
 class Weather extends React.Component {
   render() {
-    return <div>Weather Component</div>;
+    return (
+      // stex stugvuma ete orinak city === true (aysinkn datark chi ) && erkrord city === true tpuma Location ... hakarak depkum ban chi tpum
+      <div>
+        {this.props.city && this.props.country && (
+          <p>
+            Location : {this.props.city}, {this.props.country}
+          </p>
+        )}
+        {this.props.temperature && (
+          <p>Temperature : {this.props.temperature}</p>
+        )}
+        {this.props.humidity && <p>Humidity : {this.props.humidity}</p>}
+        {this.props.description && <p>Conditions : {this.props.description}</p>}
+        {this.props.erorr && <p>{this.props.erorr}</p>}
+      </div>
+    );
   }
 }
 
