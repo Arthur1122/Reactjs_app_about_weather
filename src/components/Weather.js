52ceb86591db79b81a3846cf3b85c1399e0eaaa4
Artur Petrosyan <arm.arturpetrosyan@gmail.com>
2019-11-26 23:04:59 +0400

    Change the class which function

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,24 +1,20 @@
 import React from "react";
 
-class Weather extends React.Component {
-  render() {
-    return (
-      // stex stugvuma ete orinak city === true (aysinkn datark chi ) && erkrord city === true tpuma Location ... hakarak depkum ban chi tpum
-      <div>
-        {this.props.city && this.props.country && (
-          <p>
-            Location : {this.props.city}, {this.props.country}
-          </p>
-        )}
-        {this.props.temperature && (
-          <p>Temperature : {this.props.temperature}</p>
-        )}
-        {this.props.humidity && <p>Humidity : {this.props.humidity}</p>}
-        {this.props.description && <p>Conditions : {this.props.description}</p>}
-        {this.props.erorr && <p>{this.props.erorr}</p>}
-      </div>
-    );
-  }
-}
+const Weather = props => {
+  return (
+    // stex stugvuma ete orinak city === true (aysinkn datark chi ) && erkrord city === true tpuma Location ... hakarak depkum ban chi tpum
+    <div>
+      {props.city && props.country && (
+        <p>
+          Location : {props.city}, {props.country}
+        </p>
+      )}
+      {props.temperature && <p>Temperature : {props.temperature}</p>}
+      {props.humidity && <p>Humidity : {props.humidity}</p>}
+      {props.description && <p>Conditions : {props.description}</p>}
+      {props.erorr && <p>{props.erorr}</p>}
+    </div>
+  );
+};
 
 export default Weather;
